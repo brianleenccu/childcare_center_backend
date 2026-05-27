9bc1a13c82cf2fbd6bf7fed9a717d10e73483522
Anne Lin <113306010@g.nccu.edu.tw>
2026-05-27 21:10:19 +0800

    remove password in supabase.js

--- src/core/config/supabase.js
+++ src/core/config/supabase.js
@@ -3,9 +3,8 @@ const { createClient } = require("@supabase/supabase-js"); //操作 Supabase Sto
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  // "https://rfzavcliggzlpkqqcrzr.supabase.co",
+
   process.env.SUPABASE_ANON_KEY
-  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmemF2Y2xpZ2d6bHBrcXFjcnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNzY1NjUsImV4cCI6MjA5MjY1MjU2NX0.PAPu8svIFjvDXUfY91yXGIRmktBCKExsOnqxlYW0z_I"
 );
 
 module.exports = supabase;
